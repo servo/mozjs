f7aad819d4e3682a0ff7d823c00e729b7a894628
Alan Jeffrey <ajeffrey@mozilla.com>
2018-05-10 11:00:05 -0500

    Fix CallArgs::from_vp

--- src/jsimpls.rs
+++ src/jsimpls.rs
@@ -273,6 +273,8 @@ impl JS::CallArgs {
             ),
             argc_: argc,
             argv_: vp.offset(2),
+            #[cfg(not(feature = "debugmozjs"))]
+            __bindgen_padding_0: [0, 0, 0],
             #[cfg(feature = "debugmozjs")]
             wantUsedRval_: JS::detail::IncludeUsedRval {
                 usedRval_: false,
