2e9feb76477b3c1d2424236bf4fc63a1ac3c3307
sagudev <samo.golez@outlook.com>
2021-06-04 18:45:33 +0200

    Apply patches on r2

--- mozjs/js/src/util/GetPidProvider.h
+++ mozjs/js/src/util/GetPidProvider.h
@@ -8,8 +8,15 @@
 #define util_GetPidProvider_h
 
 #ifdef XP_WIN
-#  include <process.h>
-#  define getpid _getpid
+#  ifdef JS_ENABLE_UWP
+#    define UNICODE
+#    include <Windows.h>
+#    include <processthreadsapi.h>
+#    define getpid GetCurrentProcessId
+#  else
+#    include <process.h>
+#    define getpid _getpid
+#  endif
 #elif defined(__wasi__)
 #  define getpid() 1
 #else
