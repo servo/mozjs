731b1464a0a44311c1f8a101cba1ce9a45e8614c
sagudev <16504129+sagudev@users.noreply.github.com>
2023-09-05 20:49:17 +0200

    necessary extern crates

--- mozjs/src/lib.rs
+++ mozjs/src/lib.rs
@@ -2,10 +2,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#[allow(unused_extern_crates)]
+#![allow(unused_extern_crates)]
+
+// This extern crates are needed for linking
 extern crate encoding_c;
-#[allow(unused_extern_crates)]
 extern crate encoding_c_mem;
+extern crate libz_sys;
 
 // The jsimpls module just implements traits so can be private
 mod jsimpls;
