369f2902e6481b8f237cca1652f56e61f508a0ad
Jonathan Schwender <55576758+jschwe@users.noreply.github.com>
2024-06-16 06:59:21 +0200

    Also respect readelf and objcopy cc-rs variants (#465)
    * Also respect readelf and objcopy cc-rs variants
    
    While compiling servo for OpenHarmony on a MacOS host,
    I noticed that readelf and objcopy apparently aren't chosen
    from the same directory as the clang c compiler, but needed
    to be in PATH for the configure script to pick them up.
    To allow users to set READELF and OBJCOPY explicitly, we
    should also read the relevant cc-rs environment variables.
    
    An alternative would have been to inspect the configure scripts
    and find out why they don't look next to clang, but I'm not
    familiar enough with the configure script.
    
    * Bump the sccache action from 0.0.3 to 0.0.4

--- mozjs-sys/build.rs
+++ mozjs-sys/build.rs
@@ -45,6 +45,8 @@ const SM_TARGET_ENV_VARS: &'static [&'static str] = &[
     "CPPFLAGS",
     "CXX",
     "CXXFLAGS",
+    "READELF",
+    "OBJCOPY",
 ];
 
 const EXTRA_FILES: &'static [&'static str] = &[
