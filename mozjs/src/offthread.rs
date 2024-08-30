5cb4b557b05b4a43e7f3e70200c8a024b7a255ca
sagudev <16504129+sagudev@users.noreply.github.com>
2024-08-30 12:47:07 +0200

    STACK_SIZE
    Signed-off-by: sagudev <16504129+sagudev@users.noreply.github.com>

--- mozjs/src/offthread.rs
+++ mozjs/src/offthread.rs
@@ -6,10 +6,21 @@ use crate::jsapi::JS::{
     CompileGlobalScriptToStencil2, DestroyFrontendContext, FrontendContext as RawFrontendContext,
     NewFrontendContext, ReadOnlyCompileOptions,
 };
+use crate::jsapi::{SetNativeStackQuota, ThreadStackQuotaForSize};
 use crate::rust::{transform_str_to_source_text, OwningCompileOptionsWrapper, Stencil};
 use std::ops::Deref;
 use std::sync::Arc;
 use std::thread::{self, JoinHandle};
+
+/// We want our default stack size limit to be approximately 2MB, to be safe for
+/// JS helper tasks that can use a lot of stack, but expect most threads to use
+/// much less. On Linux, however, requesting a stack of 2MB or larger risks the
+/// kernel allocating an entire 2MB huge page for it on first access, which we do
+/// not want. To avoid this possibility, we subtract 2 standard VM page sizes
+/// from our default.
+///
+/// <https://searchfox.org/mozilla-central/rev/cb5faf5dd5176494302068c553da97b4d08aa339/xpcom/threads/TaskController.cpp#43>
+const STACK_SIZE: usize = 2048 * 1024 - 2 * 4096;
 
 pub struct FrontendContext(*mut RawFrontendContext);
 
@@ -18,6 +29,12 @@ unsafe impl Send for FrontendContext {}
 impl FrontendContext {
     pub fn new() -> Self {
         Self(unsafe { NewFrontendContext() })
+    }
+
+    pub fn set_stack_quota(&self, size: usize) {
+        unsafe {
+            SetNativeStackQuota(self.0, ThreadStackQuotaForSize(size));
+        }
     }
 }
 
@@ -61,7 +78,9 @@ where
     OffThreadToken(
         thread::Builder::new()
             .name("OffThread Compile".to_string())
+            .stack_size(STACK_SIZE)
             .spawn(move || {
+                fc.set_stack_quota(STACK_SIZE);
                 callback(unsafe {
                     Stencil::from_raw(CompileGlobalScriptToStencil2(
                         *fc,
