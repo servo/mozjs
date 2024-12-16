bb560b6276cc587b4706b3162160cf945210335a
Simon Wülker <simon.wuelker@arcor.de>
2024-12-16 01:46:24 +0100

    Allow iterating over the frames of a CapturedJSStack (#539)
    * Allow iterating over the frames in a SavedJSStack
    
    Signed-off-by: Simon Wülker <simon.wuelker@arcor.de>
    
    * Add test for CapturedJSStack::for_each_stack_frame
    
    Signed-off-by: Simon Wülker <simon.wuelker@arcor.de>
    
    * Move stack iteration test into its own file
    
    This prevents other tests from interfering with the js engine
    initialization
    
    Signed-off-by: Simon Wülker <simon.wuelker@arcor.de>
    
    ---------
    
    Signed-off-by: Simon Wülker <simon.wuelker@arcor.de>

--- mozjs/tests/capture_stack.rs
+++ mozjs/tests/capture_stack.rs
@@ -13,6 +13,24 @@ use mozjs::rust::{JSEngine, RealmOptions, Runtime, SIMPLE_GLOBAL_CLASS};
 
 #[test]
 fn capture_stack() {
+    unsafe extern "C" fn print_stack(context: *mut JSContext, argc: u32, vp: *mut Value) -> bool {
+        let args = CallArgs::from_vp(vp, argc);
+
+        capture_stack!(in(context) let stack);
+        let str_stack = stack
+            .unwrap()
+            .as_string(None, StackFormat::SpiderMonkey)
+            .unwrap();
+        println!("{}", str_stack);
+        assert_eq!(
+            "bar@test.js:3:21\nfoo@test.js:5:17\n@test.js:8:16\n".to_string(),
+            str_stack
+        );
+
+        args.rval().set(UndefinedValue());
+        true
+    }
+
     let engine = JSEngine::init().unwrap();
     let runtime = Runtime::new(engine.handle());
     let context = runtime.cx();
@@ -58,22 +76,4 @@ fn capture_stack() {
             .evaluate_script(global.handle(), javascript, "test.js", 0, rval.handle_mut())
             .is_ok());
     }
-}
-
-unsafe extern "C" fn print_stack(context: *mut JSContext, argc: u32, vp: *mut Value) -> bool {
-    let args = CallArgs::from_vp(vp, argc);
-
-    capture_stack!(in(context) let stack);
-    let str_stack = stack
-        .unwrap()
-        .as_string(None, StackFormat::SpiderMonkey)
-        .unwrap();
-    println!("{}", str_stack);
-    assert_eq!(
-        "bar@test.js:3:21\nfoo@test.js:5:17\n@test.js:8:16\n".to_string(),
-        str_stack
-    );
-
-    args.rval().set(UndefinedValue());
-    true
 }
