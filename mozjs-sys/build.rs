8043910610d721f2971316b53d84c8ed93ee40b5
sagudev <16504129+sagudev@users.noreply.github.com>
2024-12-21 17:36:32 +0100

    Do not strip artifacts when DEBUGMOZJS
    Signed-off-by: sagudev <16504129+sagudev@users.noreply.github.com>

--- mozjs-sys/build.rs
+++ mozjs-sys/build.rs
@@ -888,19 +888,21 @@ fn compress_static_lib(build_dir: &Path) -> Result<(), std::io::Error> {
             &mut File::open(build_dir.join("gluebindings.rs")).unwrap(),
         )?;
     } else {
-        let strip_bin = get_cc_rs_env_os("STRIP").unwrap_or_else(|| "strip".into());
-        // Strip symbols from the static binary since it could bump up to 1.6GB on Linux.
-        // TODO: Maybe we could separate symbols for thos who still want the debug ability.
-        // https://github.com/GabrielMajeri/separate-symbols
-        let mut strip = Command::new(strip_bin);
-        if !target.contains("apple") {
-            strip.arg("--strip-debug");
-        };
-        let status = strip
-            .arg(build_dir.join("js/src/build/libjs_static.a"))
-            .status()
-            .unwrap();
-        assert!(status.success());
+        if env::var_os("CARGO_FEATURE_DEBUGMOZJS").is_none() {
+            let strip_bin = get_cc_rs_env_os("STRIP").unwrap_or_else(|| "strip".into());
+            // Strip symbols from the static binary since it could bump up to 1.6GB on Linux.
+            // TODO: Maybe we could separate symbols for thos who still want the debug ability.
+            // https://github.com/GabrielMajeri/separate-symbols
+            let mut strip = Command::new(strip_bin);
+            if !target.contains("apple") {
+                strip.arg("--strip-debug");
+            };
+            let status = strip
+                .arg(build_dir.join("js/src/build/libjs_static.a"))
+                .status()
+                .unwrap();
+            assert!(status.success());
+        }
 
         // This is the static library of spidermonkey.
         tar.append_file(
