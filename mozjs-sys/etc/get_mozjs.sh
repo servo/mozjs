1da738e83373497d0ee816e1c0db8e4ca23909d9
Josh Matthews <josh@joshmatthews.net>
2025-05-29 02:58:58 -0400

    Update mozilla-esr references and package version.
    Signed-off-by: Josh Matthews <josh@joshmatthews.net>

--- mozjs-sys/etc/get_mozjs.sh
+++ mozjs-sys/etc/get_mozjs.sh
@@ -4,7 +4,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-REPO=mozilla-esr128
+REPO=mozilla-release
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # get commit and appropriet mozjs tar
