6d5901ca73132feb59e38579f7e06168cdbefb9d
Josh Matthews <josh@joshmatthews.net>
2025-08-09 23:17:18 -0400

    Fetch mozjs source from github release. Reenable integrity check.
    Signed-off-by: Josh Matthews <josh@joshmatthews.net>

--- mozjs-sys/etc/get_mozjs.sh
+++ mozjs-sys/etc/get_mozjs.sh
@@ -4,16 +4,10 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-REPO=mozilla-release
-
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-# get commit and appropriet mozjs tar
+# get commit and appropriate mozjs tar
 COMMIT=$( cat $SCRIPT_DIR/COMMIT )
 echo "Commit $COMMIT"
-job_id=$(curl "https://treeherder.mozilla.org/api/project/$REPO/push/?revision=$COMMIT" | jq '.results[0].id')
-echo "Job id $job_id"
-task_id=$(curl "https://treeherder.mozilla.org/api/jobs/?push_id=$job_id" | jq -r '.results[] | select(.[] == "spidermonkey-sm-package-linux64/opt") | .[14]')
-echo "Task id $task_id"
-tar_file=$(curl "https://firefox-ci-tc.services.mozilla.com/api/queue/v1/task/$task_id/runs/0/artifacts" | jq -r '.artifacts[] | select(.name | contains("tar.xz")) | .name')
-echo "Tar at https://firefox-ci-tc.services.mozilla.com/api/queue/v1/task/$task_id/runs/0/artifacts/$tar_file"
-curl -L --output mozjs.tar.xz "https://firefox-ci-tc.services.mozilla.com/api/queue/v1/task/$task_id/runs/0/artifacts/$tar_file"
+tar_file=$(curl -L -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/servo/mozjs/releases/tags/mozjs-source-$COMMIT | jq -r '.assets[] | select(.name | contains("tar.xz")) | .browser_download_url')
+echo "Tar at $tar_file"
+curl -L --output mozjs.tar.xz $tar_file
