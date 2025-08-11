0bebd6872b1a061b69bcfcf73162abf0af27eebc
Narfinger <Narfinger@users.noreply.github.com>
2025-08-11 13:18:51 +0200

    Improvements to the speed of latin1_to_string. (#587)
    On the benchmark it improves the encoding from around 9 microsends to 6
    microseconds.
    
    This PR also includes the benchmark and setup as criterion benchmark.
    
    Also test the higher parts of latin1 which are not the same.
    
    At the moment we have to use encoding_rs to do this encoding leading us
    to a bit of a cyclical testing. But tests should prepare for future
    refactoring, so I think this is fine.
    
    Signed-off-by: Narfinger <Narfinger@users.noreply.github.com>

--- mozjs/tests/external_string.rs
+++ mozjs/tests/external_string.rs
@@ -37,23 +37,44 @@ fn external_string() {
         ));
         let _ac = JSAutoRealm::new(context, global.get());
 
-        let latin1_base = "test latin-1";
-        let latin1_boxed = latin1_base.as_bytes().to_vec().into_boxed_slice();
-        let latin1_chars = Box::into_raw(latin1_boxed).cast::<u8>();
-
-        let callbacks = CreateJSExternalStringCallbacks(
-            &EXTERNAL_STRING_CALLBACKS_TRAPS,
-            latin1_base.len() as *mut c_void,
-        );
-        rooted!(in(context) let latin1_jsstr = JS_NewExternalStringLatin1(
+        test_latin1_string(context, "test latin1");
+        test_latin1_string(context, "abcdefghijklmnop"); // exactly 16 bytes
+        test_latin1_string(context, "abcdefghijklmnopq"); // 17 bytes
+        test_latin1_string(context, "abcdefghijklmno"); // 15 bytes
+        test_latin1_string(context, "abcdefghijklmnopqrstuvwxyzabcdef"); //32 bytes
+        test_latin1_string(context, "abcdefghijklmnopqrstuvwxyzabcde"); //31 bytes
+        test_latin1_string(context, "abcdefghijklmnopqrstuvwxyzabcdefg"); //33 bytes
+                                                                          //test_latin1_string(context, "test latin-1 Ö"); //testing whole latin1 range.
+                                                                          // whole latin1 table
+        test_latin1_string(context, "   ! 	\" 	# 	$ 	% 	& 	' 	( 	) 	* 	+ 	, 	- 	. 	/");
+        test_latin1_string(context, "0 	1 	2 	3 	4 	5 	6 	7 	8 	9 	: 	; 	< 	= 	> 	?");
+        test_latin1_string(context, "@ 	A 	B 	C 	D 	E 	F 	G 	H 	I 	J 	K 	L 	M 	N 	O");
+        test_latin1_string(context, "P 	Q 	R 	S 	T 	U 	V 	W 	X 	Y 	Z 	[ 	\\ 	] 	^ 	_");
+        test_latin1_string(context, "` 	a 	b 	c 	d 	e 	f 	g 	h 	i 	j 	k 	l 	m 	n 	o");
+        test_latin1_string(context, "p 	q 	r 	s 	t 	u 	v 	w 	x 	y 	z 	{ 	| 	} 	~");
+        test_latin1_string_bytes(
             context,
-            latin1_chars,
-            latin1_base.len(),
-            callbacks
-        ));
-        assert_eq!(
-            jsstr_to_string(context, NonNull::new(latin1_jsstr.get()).unwrap()),
-            latin1_base
+            b"\xA0\xA1\xA2\xA3\xA4\xA5\xA6\xA7\xA8\xA9\xAA\xAB\xAC\xAD\xAE\xAF",
+        );
+        test_latin1_string_bytes(
+            context,
+            b"\xB0\xB1\xB2\xB3\xB4\xB5\xB6\xB7\xB8\xB9\xBA\xBB\xBC\xBD\xBE\xBF",
+        );
+        test_latin1_string_bytes(
+            context,
+            b"\xC0\xC1\xC2\xC3\xC4\xC5\xC6\xC7\xC8\xC9\xCA\xCB\xCC\xCD\xCE\xCF",
+        );
+        test_latin1_string_bytes(
+            context,
+            b"\xD0\xD1\xD2\xD3\xD4\xD5\xD6\xD7\xD8\xD9\xDA\xDB\xDC\xDD\xDE\xDF",
+        );
+        test_latin1_string_bytes(
+            context,
+            b"\xE0\xE1\xE2\xE3\xE4\xE5\xE6\xE7\xE8\xE9\xEA\xEB\xEC\xED\xEE\xEF",
+        );
+        test_latin1_string_bytes(
+            context,
+            b"\xF0\xF1\xF2\xF3\xF4\xF5\xF6\xF7\xF8\xF9\xFA\xFB\xFC\xFD\xFE\xFF",
         );
 
         let utf16_base = "test utf-16 $€ \u{10437}\u{24B62}";
@@ -79,6 +100,48 @@ fn external_string() {
             utf16_base
         );
     }
+}
+
+#[cfg(test)]
+unsafe fn test_latin1_string(context: *mut mozjs::jsapi::JSContext, latin1_base: &str) {
+    let latin1_boxed = latin1_base.as_bytes().to_vec().into_boxed_slice();
+    let latin1_chars = Box::into_raw(latin1_boxed).cast::<u8>();
+
+    let callbacks = CreateJSExternalStringCallbacks(
+        &EXTERNAL_STRING_CALLBACKS_TRAPS,
+        latin1_base.len() as *mut c_void,
+    );
+    rooted!(in(context) let latin1_jsstr = JS_NewExternalStringLatin1(
+        context,
+        latin1_chars,
+        latin1_base.len(),
+        callbacks
+    ));
+    assert_eq!(
+        jsstr_to_string(context, NonNull::new(latin1_jsstr.get()).unwrap()),
+        latin1_base
+    );
+}
+
+#[cfg(test)]
+unsafe fn test_latin1_string_bytes(context: *mut mozjs::jsapi::JSContext, latin1_base: &[u8]) {
+    let latin1_boxed = latin1_base.to_vec().into_boxed_slice();
+    let latin1_chars = Box::into_raw(latin1_boxed).cast::<u8>();
+
+    let callbacks = CreateJSExternalStringCallbacks(
+        &EXTERNAL_STRING_CALLBACKS_TRAPS,
+        latin1_base.len() as *mut c_void,
+    );
+    rooted!(in(context) let latin1_jsstr = JS_NewExternalStringLatin1(
+        context,
+        latin1_chars,
+        latin1_base.len(),
+        callbacks
+    ));
+    assert_eq!(
+        jsstr_to_string(context, NonNull::new(latin1_jsstr.get()).unwrap()),
+        encoding_rs::mem::decode_latin1(latin1_base)
+    );
 }
 
 static EXTERNAL_STRING_CALLBACKS_TRAPS: JSExternalStringCallbacksTraps =
