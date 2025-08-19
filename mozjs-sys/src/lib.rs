4bfff87b8ce750eea9cd294bc82608efbd85499a
Redfire <redfire75369@hotmail.com>
2025-08-19 21:30:14 +0800

    Added Cargo Feature for zlib-rs
    Signed-off-by: Redfire <redfire75369@hotmail.com>

--- mozjs-sys/src/lib.rs
+++ mozjs-sys/src/lib.rs
@@ -11,7 +11,10 @@
 extern crate encoding_c;
 extern crate encoding_c_mem;
 extern crate icu_capi;
+#[cfg(feature = "libz-rs")]
 extern crate libz_rs_sys;
+#[cfg(feature = "libz-sys")]
+extern crate libz_sys;
 
 // The jsimpls module just implements traits so can be private
 mod jsimpls;
