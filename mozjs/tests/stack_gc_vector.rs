5ed845beb6749ffdaa6833afa14718fe5abee3ad
Josh Matthews <josh@joshmatthews.net>
2025-08-28 01:58:45 -0400

    tmp: narrow down windows crash
    Signed-off-by: Josh Matthews <josh@joshmatthews.net>

--- mozjs/tests/stack_gc_vector.rs
+++ mozjs/tests/stack_gc_vector.rs
@@ -32,7 +32,7 @@ unsafe extern "C" fn content_security_policy_allows(
     _body_arg: HandleValue,
     can_compile_strings: *mut bool,
 ) -> bool {
-    let parameter_strings = SafeHandle::from_raw(parameter_strings);
+    /*let parameter_strings = SafeHandle::from_raw(parameter_strings);
     assert_eq!(parameter_strings.len(), 1);
     let string0 = parameter_strings.at(0).expect("should have a value");
     let string0 = NonNull::new(*string0).expect("should be non-null");
@@ -43,7 +43,7 @@ unsafe extern "C" fn content_security_policy_allows(
     let arg0 = parameter_args.at(0).expect("should have a value");
     let string0 = arg0.to_string();
     let string0 = NonNull::new(string0).expect("should be non-null");
-    assert_eq!(jsstr_to_string(cx, string0), "a".to_string());
+    assert_eq!(jsstr_to_string(cx, string0), "a".to_string());*/
 
     *RAN_CSP_CALLBACK.lock().unwrap() = true;
     *can_compile_strings = true;
