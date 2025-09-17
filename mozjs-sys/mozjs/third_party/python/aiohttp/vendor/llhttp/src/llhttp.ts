f5d9a371d55935c6184dece01ae3deb13c99a872
Jonathan Schwender <55576758+jschwe@users.noreply.github.com>
2025-09-17 10:14:13 +0800

    Prepare mozjs-sys for publishing (#617)
    * Update mozjs_sys publishing excludes
    
    Most of the previous folders don't exist anymore.
    `testing` is 60MiB uncompressed, so we don't want
    to ship that.
    
    Signed-off-by: Jonathan Schwender <schwenderjonathan@gmail.com>
    
    * Prevent python bytecode cache
    
    Cargo will complain when publishing if the source directory
    is polluted during build.
    
    Signed-off-by: Jonathan Schwender <schwenderjonathan@gmail.com>
    
    * Remove old-configure from makefile.cargo
    
    The file doesn't exist anymore, so we shouldn't touch it.
    This prevents cargo publish from complaining about failed
    package verification.
    
    Cargo will complain when publishing if the source directory
    is polluted during build.
    
    Signed-off-by: Jonathan Schwender <schwenderjonathan@gmail.com>
    
    * Update authors for mozjs-sys
    
    Add the servo project developers to the authors.
    This should make it more clear that this is not an official mozilla crate.
    We don't remove Mozilla from the list, since they are the primary
    authors of spidermonkey, aka. the main content of this crate.
    
    Signed-off-by: Jonathan Schwender <schwenderjonathan@gmail.com>
    
    * filter repo instead of excluding on publish
    
    Signed-off-by: Jonathan Schwender <schwenderjonathan@gmail.com>
    
    * bump version
    
    Signed-off-by: Jonathan Schwender <schwenderjonathan@gmail.com>
    
    ---------
    
    Signed-off-by: Jonathan Schwender <schwenderjonathan@gmail.com>

--- mozjs-sys/mozjs/third_party/python/aiohttp/vendor/llhttp/src/llhttp.ts
+++
@@ -1,7 +0,0 @@
-import * as constants from './llhttp/constants';
-
-export { constants };
-
-export { HTTP } from './llhttp/http';
-export { URL } from './llhttp/url';
-export { CHeaders } from './llhttp/c-headers';
