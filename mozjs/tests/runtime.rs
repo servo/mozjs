95e90d33b7de58e6569abbea08150ce4b0b94b41
Till Schneidereit <till@tillschneidereit.net>
2025-09-30 22:05:33 +0200

    Support building for Wasm/WASI
    This commit changes the `build.rs` and `makefile.cargo` files to support compiling mozjs for `wasm32-wasip1` and `wasm32-wasip2`. SpiderMonkey itself has long supported targeting WASI, but the build system requires some finagling to line everything up just right for these targets.
    
    It also frobs things a little bit to make tests pass (disabling the one test that uses threads and thus isn't supported at all on wasm32-wasi right now.)
    
    Signed-off-by: Till Schneidereit <till@tillschneidereit.net>

--- mozjs/tests/runtime.rs
+++ mozjs/tests/runtime.rs
@@ -2,6 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#![cfg(not(target_arch = "wasm32"))]
+
 use std::ptr;
 use std::sync::mpsc::channel;
 use std::thread;
