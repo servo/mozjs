b50a0f642dcbcd8c9226acaac7ba1c2576abda63
Simon Wülker <simon.wuelker@arcor.de>
2026-04-08 08:23:04 +0200

    mozjs-sys: Build from source if the jitspew feature is enabled (#728)
    Otherwise the `IONFLAGS` environment variable does nothing.
    
    Testing: We don't have tests for the build process
    Servo companion PR: https://github.com/servo/servo/pull/44010
    
    ---------
    
    Signed-off-by: Simon Wülker <simon.wuelker@arcor.de>

--- mozjs-sys/build.rs
+++ mozjs-sys/build.rs
@@ -475,6 +475,9 @@ fn should_build_from_source() -> bool {
     } else if !env::var_os("CARGO_FEATURE_JIT").is_some() {
         println!("jit feature is NOT enabled. Building from source directly.");
         true
+    } else if env::var_os("CARGO_FEATURE_JITSPEW").is_some() {
+        println!("jitspew feature is enabled. Building from source directly.");
+        true
     } else {
         false
     }
