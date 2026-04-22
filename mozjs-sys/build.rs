ef6785a6a1d01ea9415a8546cf66822f52f653b0
Jonathan Schwender <schwenderjonathan@gmail.com>
2026-04-22 14:36:17 +0200

    link to issue.
    Signed-off-by: Jonathan Schwender <schwenderjonathan@gmail.com>

--- mozjs-sys/build.rs
+++ mozjs-sys/build.rs
@@ -226,6 +226,8 @@ fn build_spidermonkey(build_dir: &Path) {
     // Unfortunately, the old pipe jobserver that cargo exposes, has a new bug in 4.4,
     // which is exposed by spidermonkeys makefiles and makes compilation largely
     // single-threaded.
+    // The workaround can be removed / adapted once cargo supports the new jobserver:
+    // <https://github.com/rust-lang/cargo/issues/13483>
     if is_buggy_make_version() {
         // worst-case we'll get 2xNUM_JOBS jobs, since we will have two independant job servers
         let num_jobs = env::var("NUM_JOBS").expect("NUM_JOBS should be set by cargo");
