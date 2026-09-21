92342c0531ac9fcea4b07c2fa1443c99ca5ea72c
sagudev <16504129+sagudev@users.noreply.github.com>
2026-09-21 11:02:11 +0200

    move commiting mozjs-extracted-crates into update.py
    Signed-off-by: sagudev <16504129+sagudev@users.noreply.github.com>

--- mozjs-sys/etc/update.py
+++ mozjs-sys/etc/update.py
@@ -279,7 +279,8 @@ impl icu_casemap::ClosureSink for CodePointInversionListBuilder {
 
     if commit:
         subprocess.check_call(
-            ["git", "add", "--all", TARGET], stdout=subprocess.DEVNULL
+            ["git", "add", "--all", TARGET, "mozjs-extracted-crates"],
+            stdout=subprocess.DEVNULL,
         )
         subprocess.check_call(
             ["git", "commit", "-s", "-m", "Update SpiderMonkey"],
